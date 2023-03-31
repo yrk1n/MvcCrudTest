b41cc1e8d3f205410ee793dc3a1db44bb309e7bf
yrk1n <yarkin.kanli@bilgiedu.net>
2023-03-31 19:11:34 +0300

    transaction page updated

--- MvcCrudTest/Models/Transaction.cs
+++ MvcCrudTest/Models/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcCrudTest.Models
@@ -10,15 +11,19 @@ namespace MvcCrudTest.Models
         public int TransactionId { get; set; }
 
         [Column(TypeName = "varchar(12)")]
+        [DisplayName("Account Number")]
         public string AccountNumber { get; set; }
 
         [Column(TypeName = "varchar(100)")]
+        [DisplayName("Beneficiary Name")]
         public string BeneficiaryName { get; set; }
 
         [Column(TypeName = "varchar(100)")]
+        [DisplayName("Bank Name")]
         public string BankName { get; set; }
 
         [Column(TypeName = "varchar(11)")]
+        [DisplayName("SWIFT Code")]
         public string SWIFTCode { get; set; }
 
 
