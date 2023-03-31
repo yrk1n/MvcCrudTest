b69753ecfc819df6a4a09efff964954685c217b4
yrk1n <yarkin.kanli@bilgiedu.net>
2023-03-31 15:03:06 +0300

    Db connection string configured and first migration

--- MvcCrudTest/Program.cs
+++ MvcCrudTest/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MvcCrudTest.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+//Dependency Injection for DbContext
+builder.Services.AddDbContext<TransactionDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
